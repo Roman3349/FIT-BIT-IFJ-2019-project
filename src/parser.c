9b0fb6259f413fcd8f7db8dded7ead367405adec
Frantisek Jerabek <fanajerabek@seznam.cz>
2019-11-20 10:14:39 +0100

    Added while parser, assignment parser. Added token to readable string conversion. !! Temporary expression parser !!

--- src/parser.c
+++ src/parser.c
@@ -19,71 +19,163 @@
 #include "parser.h"
 
 //#include "scanner.h"
-token_t* current_token = NULL;
 
-token_t next_token(FILE* file, intStack_t* stack) {
-    *current_token = scan(file, stack);
-    return *current_token;
-}
+tree_element_t syntax_parse(FILE* file) {
 
-token_t peek_token(FILE* file, intStack_t* stack){
-    if (current_token == NULL) {
-        return next_token(file, stack);
-    }
-    return *current_token;
-}
+    intStack_t* stack = stackInit();
 
+//    token_t token;
+    parse_while(file, stack);
 
-tree_element_t syntax_parse (FILE* file) {
-//    intStack_t* stack = stackInit();
-    tree_el_data_t data = { .d = 1234};
-    tree_element_t tree = { .data = &data};
+//    printf("Last token: %s\n", token_to_string(token));
 
-
-    printf("current_token %d \n", current_token->type);
+    tree_el_data_t data = { .d=1000 };
+    tree_element_t tree = { .data = &data };
     return tree;
 }
 
-bool function_param_g(FILE* file, intStack_t* stack) {
-    return expression_g(file, stack);
+
+bool parse_expression(FILE* file, intStack_t* stack){
+    printf("%s\n", token_to_string(scan(file, stack)));
+    printf("%s\n", token_to_string(scan(file, stack)));
+    printf("%s\n", token_to_string(scan(file, stack)));
+    return true; //FIXME: Expression grammar parser (temporary scrap for 3 tokens)
 }
 
-bool function_nparam_g(FILE* file, intStack_t* stack) {
-
-    token_t token = peek_token(file, stack);
-    if(token.type != T_STRING || token.data.intval != ',') return false;//FIXME Check for COMMA token
-    token = next_token(file, stack);
-    if(function_param_g(file, stack)) return true;
-    return true;
-}
-
-bool function_params_g(FILE* file, intStack_t* stack) {
-    if(!function_param_g(file, stack)) return false;
-    function_nparam_g(file, stack);
-    return true;
-
-}
-
-bool function_call_g(FILE* file, intStack_t* stack) {
-    if(peek_token(file, stack).type != T_ID) return false;
-    if(next_token(file, stack).type != T_LPAR) return false;
-    //TODO: function_params_g
-    return true;
-}
-
-bool eols_g (FILE* file, intStack_t* stack) {
-    while(peek_token(file, stack).type == T_EOL) {
-        next_token(file, stack);
-    }
-    return true;
-}
-
-bool expression_g (FILE* file, intStack_t* stack) {
-    token_t token = peek_token(file, stack);
-    if(token.type == T_NONE || token.type == T_ID || token.type == T_NUMBER || token.type == T_STRING) { //TODO: Grammar for math expressions
-        next_token(file, stack);
-        return true;
-    } else {
+bool parse_while(FILE* file, intStack_t* stack) {
+    if(!parse_expression(file, stack)) {
         return false;
     }
+
+    token_t token = scan(file, stack);
+    if(token.type != T_COLON) {
+        fprintf(stderr, "Syntax error. Expected colon, got %s \n", token_to_string(token));
+        return false;
+    }
+
+    token = scan(file, stack);
+    if(token.type != T_EOL) {
+        fprintf(stderr, "Syntax error. Expected EOL, got %s \n", token_to_string(token));
+        return false;
+    }
+
+    token = scan(file, stack);
+    if(token.type != T_INDENT) {
+        fprintf(stderr, "Syntax error. Expected indentation, got %s \n", token_to_string(token));
+        return false;
+    }
+
+    if(!parse_block(file, stack, &token)) { //TODO: block parser
+        return false;
+    }
+
+    if(token.type != T_DEDENT) {
+        fprintf(stderr, "Syntax error. Invalid indentation near %s token.\n", token_to_string(token));
+        return false;
+    }
+
+    return true;
+}
+
+bool parse_assignment(FILE* file, intStack_t* stack) {
+    if(!parse_expression(file, stack)){
+        return false;
+    }
+
+    token_t token = scan(file, stack);
+    if(token.type == T_EOL) {
+        fprintf(stderr, "Syntax error. Expected EOL, got  %s token.\n", token_to_string(token));
+        return false;
+    }
+
+    return true;
+}
+
+char* token_to_string (token_t token) {
+    switch(token.type) {
+
+        case T_EOL:
+            return "EOL";
+        case T_EOF:
+            return "EOF";
+        case T_OP_NEG:
+            return "'!='";
+        case T_OP_ADD:
+            return "'+'";
+        case T_OP_SUB:
+            return "'-'";
+        case T_OP_MUL:
+            return "'*'";
+        case T_OP_DIV:
+            return "'/'";
+        case T_OP_EQ:
+            return "'=='";
+        case T_OP_GREATER:
+            return "'>'";
+        case T_OP_LESS:
+            return "'<'";
+        case T_OP_GREATER_EQ:
+            return "'>='";
+        case T_OP_LESS_EQ:
+            return "'<='";
+        case T_OP_NOT_EQ:
+            return "'!='";
+        case T_BOOL_AND:
+            return "'and'";
+        case T_BOOL_OR:
+            return "'or'";
+        case T_BOOL_NEG:
+            return "'not'";
+        case T_NUMBER:
+            return "NUMBER";
+        case T_FLOAT:
+            return "FLOAT";
+        case T_STRING:
+            return "string";
+        case T_STRING_ML:
+            return "multiline string";
+        case T_ID:
+            return "identifier";
+        case T_KW_DEF:
+            return "'def'";
+        case T_KW_IF:
+            return "'if'";
+        case T_KW_ELSE:
+            return "'else'";
+        case T_KW_WHILE:
+            return "'while'";
+        case T_KW_PASS:
+            return "'pass'";
+        case T_KW_RETURN:
+            return "'return'";
+        case T_ASSIGN:
+            return "'='";
+        case T_COLON:
+            return "':'";
+        case T_COMMA:
+            return "','";
+        case T_LPAR:
+            return "'('";
+        case T_RPAR:
+            return "')'";
+        case T_LBRACKET:
+            return "[";
+        case T_RBRACKET:
+            return "]";
+        case T_LBRACE:
+            return "{";
+        case T_RBRACE:
+            return "}";
+        case T_INDENT:
+            return "indentation";
+        case T_DEDENT:
+            return "dedentation";
+        case T_UNKNOWN:
+            return "unknown";
+        case T_NONE:
+            return "'none'";
+        case T_ERROR:
+            return "'error'";
+    }
+    return "";
 }
--- src/parser.h
+++ src/parser.h
@@ -13,6 +13,7 @@
 
 typedef union tree_el_data tree_el_data_t;
 
+enum State { FUNCTION_DEF, BLOCK, ROOT };
 // Derivation tree element
 typedef struct{
     tree_el_data_t* data;
@@ -33,16 +34,38 @@ union tree_el_data {
  */
 tree_element_t syntax_parse(FILE * file);
 
-bool function_param_g(FILE* file, intStack_t* stack);
 
-bool function_nparam_g(FILE* file, intStack_t* stack);
+/**
+ * Parses while structure after while keyword
+ * @param file source file
+ * @param stack lexical analysis stack
+ * @return parsing successful
+ */
+bool parse_while(FILE* file, intStack_t* stack);
 
-bool function_params_g(FILE* file, intStack_t* stack);
+/**
+ * Returns string representation of token
+ * @param token token
+ * @return string representation
+ */
+char* token_to_string(token_t token);
 
-bool function_call_g(FILE* file, intStack_t* stack);
+/**
+ * Parses code block
+ * @param file source file
+ * @param stack lexical analysis stack
+ * @param last_token pointer for writing first non-block token
+ * @return parsing successful
+ */
+bool parse_block(FILE* file, intStack_t* stack, token_t* last_token);
 
-bool eols_g(FILE* file, intStack_t* stack);
+/**
+ * Parses assignment expression
+ * @param file source file
+ * @param stack lexical analysis stack
+ * @return parsing successful
+ */
+bool parse_assignment(FILE* file, intStack_t* stack);
 
-bool expression_g(FILE* file, intStack_t* stack);
 
 #endif //FIT_BIT_IFJ_2019_PROJECT_PARSER_H
